2319597e2ef3289a761300fe922108c6dff9d43d
rafaelreisramos <rrramos@gmail.com>
2022-11-21 15:35:51 -0300

    feature: add progress bar and loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
+import NextNProgress from 'nextjs-progressbar'
 import '../styles/global.css'
 
 import { AppProps } from 'next/app'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress color="#cc79a7" />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
