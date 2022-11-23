be455d6056961a78c5f1ca7ebfd42630365ac31d
rafaelreisramos <rrramos@gmail.com>
2022-11-22 21:47:05 -0300

    feature: add google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -6,6 +9,9 @@ module.exports = {
       colors: {
         primary: '#cc79a7',
         background: '#030518',
+      },
+      fontFamily: {
+        sans: ['Roboto', ...fontFamily.sans],
       },
     },
   },
