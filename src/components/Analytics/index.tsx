7ad35758ef683a5c588dc19ad5dbe8c70411712f
rafaelreisramos <rrramos@gmail.com>
2022-11-25 20:27:07 -0300

    fix: replace html script by next/script

--- src/components/Analytics/index.tsx
+++ src/components/Analytics/index.tsx
@@ -1,22 +1,22 @@
+import Script from 'next/script'
+
 export default function Analytics() {
   return (
     <>
-      <script
-        async
+      <Script
+        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING}`}
       />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', ${process.env.NEXT_PUBLIC_GA_TRACKING}, {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${process.env.NEXT_PUBLIC_GA_TRACKING}, {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
     </>
   )
 }
